b273d7a6ce12155cb21e309946ee878c08ec36dd
samwisee <mboos@outlook.com>
2017-11-27 12:01:55 +0100

    generating icons for style

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,34 +1,48 @@
 import os
 import sys
 import shutil
+import json
 from xml.sax.saxutils import unescape
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from core import process, write_styles
+from core import *
+from core import _create_icons
+
+
+def test_icon_creation():
+    image_data = _load_file(os.path.join(os.path.dirname(__file__), "..", "sample_data", "sprite.png"), binary=True)
+    image_data = base64.b64encode(image_data)
+    image_definition_data = _load_file(os.path.join(os.path.dirname(__file__), "..", "sample_data", "sprite.json"))
+    image_definition_data = json.loads(str(image_definition_data))
+    output_directory = os.path.join(os.path.dirname(__file__), "generated")
+    _create_icons(image_data, image_definition_data, output_directory=output_directory)
 
 
 def test_generate_qgis():
+    return
     # path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "osm_bright.json")
     # path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "klokantech_basic.json")
     path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "positron.json")
     data = _load_file(path)
+    data = json.loads(data)
     output_directory = r"C:\DEV\Vector-Tiles-Reader-QGIS-Plugin\styles"
     if os.path.isdir(output_directory):
         shutil.rmtree(output_directory)
-    styles = process(data)
-    write_styles(styles, output_directory)
+    generate_styles(data, output_directory)
 
 
 def test_generate_local():
+    return
     # path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "osm_bright.json")
     # path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "klokantech_basic.json")
     path = os.path.join(os.path.dirname(__file__), "..", "sample_data", "positron.json")
     data = _load_file(path)
+    data = json.loads(data)
     output_directory = os.path.join(os.path.dirname(__file__), "generated")
     if os.path.isdir(output_directory):
         shutil.rmtree(output_directory)
     styles = process(data)
-    write_styles(styles, output_directory)
+    generate_styles(data, output_directory)
 
 
 def test_filter():
@@ -41,7 +55,10 @@ def test_filter():
     assert unescape(styles[0]["rule"], entities={"&quot;": '"'}) == "\"class\" is not null and \"class\" = \'national_park\'"
 
 
-def _load_file(path):
-    with open(path, 'r') as f:
+def _load_file(path, binary=False):
+    mode = 'r'
+    if binary:
+        mode = 'rb'
+    with open(path, mode) as f:
         data = f.read()
     return data
